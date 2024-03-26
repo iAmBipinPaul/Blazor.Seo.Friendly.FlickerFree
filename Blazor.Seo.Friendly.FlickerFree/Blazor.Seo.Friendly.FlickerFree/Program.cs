59aa49eb9053c970639ac4e4e2efc47056b098c3
Bipin Paul <iambipinpaul@outlook.com>
2024-03-26 09:44:59 +0545

    add missing AddHttpContextAccessor service registration

--- Blazor.Seo.Friendly.FlickerFree/Blazor.Seo.Friendly.FlickerFree/Program.cs
+++ Blazor.Seo.Friendly.FlickerFree/Blazor.Seo.Friendly.FlickerFree/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
-
+builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
